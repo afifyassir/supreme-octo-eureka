4f9c788fa048f6bed4f283d77a55a3c0a95441c6
Yassir <afif.yassir12@gmail.com>
2023-12-15 10:30:23 +0100

    d

--- api/ml_api/persistence/data_access.py
+++ api/ml_api/persistence/data_access.py
@@ -14,6 +14,10 @@ from project.api import (
 )
 from gradient_boosting_model.predict import make_prediction as make_shadow_prediction
 
+# Set up a logger instance in Python using the logging module.
+# This is a method provided by the logging module. It returns a logger instance associated with the
+# specified name. If a logger with that name already exists, it will return the existing instance; otherwise,
+# it will create a new one.
 _logger = logging.getLogger('mlapi')
 
 
@@ -23,24 +27,35 @@ SECONDARY_VARIABLES_TO_RENAME = {
     "ThreeSsnPortch": "3SsnPorch",
 }
 
-
+# ModelType is an enumeration that defines constants for different types of models.
+# It uses Python's enum module to create an Enum class. Each member of the Enum
+# has a name (e.g., LASSO, GRADIENT_BOOSTING) and a value (e.g., "lasso", "gradient_boosting").
+# Enumerations are used to create a set of named constants that can make code more readable and less error-prone.
 class ModelType(enum.Enum):
     LASSO = "lasso"
     GRADIENT_BOOSTING = "gradient_boosting"
 
-
+# PredictionResult is a named tuple that is used to store the results of a prediction. Named
+# tuples are a subclass of tuples that allow you to access elements by name instead of by index,
+# which can make code more readable. In this case, PredictionResult has three fields: errors,
+# predictions, and model_version.
 class PredictionResult(t.NamedTuple):
     errors: t.Any
     predictions: np.array
     model_version: str
 
-
+# MODEL_PREDICTION_MAP is a dictionary that maps each ModelType to a corresponding prediction function.
+# This allows you to dynamically select the prediction function based on the model type. For example,
+# if you have a ModelType.LASSO, you can use MODEL_PREDICTION_MAP[ModelType.LASSO] to get
+# the make_live_prediction function.
 MODEL_PREDICTION_MAP = {
     ModelType.GRADIENT_BOOSTING: make_shadow_prediction,
     ModelType.LASSO: make_live_prediction,
 }
 
-
+# The PredictionPersistence class is designed to handle the storage of machine learning model
+# predictions into a database. It has two main responsibilities: making predictions using
+# the specified model and saving those predictions.
 class PredictionPersistence:
     def __init__(self, *, db_session: Session, user_id: str = None) -> None:
         self.db_session = db_session
@@ -52,22 +67,36 @@ class PredictionPersistence:
     def make_save_predictions(
         self, *, db_model: ModelType, input_data: t.List
     ) -> PredictionResult:
-        """Get the prediction from a given model and persist it."""
-        # Access the model prediction function via mapping
+        """
+        The make_save_predictions function within the PredictionPersistence class
+        is responsible for two main tasks: making predictions using a specified
+        machine learning model and saving those predictions to a database.
+        """
+        # Checking the type of model (db_model) for which predictions are to be made.
+        # If the model is of type LASSO, it performs a renaming of certain input columns.
+        # This is necessary to maintain compatibility with the expected input format of the LASSO model.
         if db_model == ModelType.LASSO:
-            # we have to rename a few of the columns for backwards
-            # compatibility with the regression model package.
             live_frame = pd.DataFrame(input_data)
+            # The method to_dict is called on the DataFrame to convert it into a dictionary.
+            # The orient='records' parameter means that each row of the DataFrame will be converted
+            # into a dictionary where the keys are the column names and the values are the row values.
+            # The result is a list of dictionaries, with each dictionary representing a single record
+            # (row) from the DataFrame.
             input_data = live_frame.rename(
                 columns=SECONDARY_VARIABLES_TO_RENAME
             ).to_dict(orient="records")
 
+        # Using a mapping (MODEL_PREDICTION_MAP) to find the correct prediction function
+        # based on the model type. This function is called with the provided input_data to
+        # generate predictions.
         result = MODEL_PREDICTION_MAP[db_model](input_data=input_data)
+
+        # retrieve any errors from the prediction results. If there are no errors, proceed to
+        # format the predictions into a list.
         errors = None
         try:
             errors = result["errors"]
         except KeyError:
-            # regression model `make_prediction` does not include errors
             pass
 
         prediction_result = PredictionResult(
@@ -76,6 +105,8 @@ class PredictionPersistence:
             model_version=result.get("version"),
         )
 
+        # If there are no errors, the function calls the save_predictions method to save the prediction
+        # results to the database.
         if prediction_result.errors:
             return prediction_result
 
@@ -92,7 +123,15 @@ class PredictionPersistence:
         prediction_result: PredictionResult,
         db_model: ModelType,
     ) -> None:
-        """Persist model predictions to storage."""
+        """
+        This function creates new database record, which corresponds to either a LassoModelPredictions
+        or GradientBoostingModelPredictions object, depending on the model type (db_model) used for
+        the prediction.This record includes several pieces of information:
+            - user_id: The identifier of the user for whom the prediction was made. This could be a real user ID or a placeholder if the user is anonymous.
+            - model_version: The version of the model that generated the prediction, as stored in the PredictionResult.
+            - inputs: The input data that was fed into the model, serialized into a JSON format.
+            - outputs: The predictions made by the model, also serialized into a JSON format.
+        """
         if db_model == db_model.LASSO:
             prediction_data = LassoModelPredictions(
                 user_id=self.user_id,
@@ -107,7 +146,8 @@ class PredictionPersistence:
                 inputs=json.dumps(inputs),
                 outputs=json.dumps(prediction_result.predictions),
             )
-
+        # Add the new record to the database session.
         self.db_session.add(prediction_data)
+        # Commit the session in order to save the record to the database.
         self.db_session.commit()
         _logger.debug(f"saved data for model: {db_model}")
